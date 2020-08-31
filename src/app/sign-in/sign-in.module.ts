73bb565eddbe46baae95e9e0d65f2e1b05e075ab
Jesus <jes.librado@gmail.com>
2020-08-31 03:38:59 -0500

    Added sign-in form

--- src/app/sign-in/sign-in.module.ts
+++ src/app/sign-in/sign-in.module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { SignInRoutingModule } from './sign-in.routing';
 import { SignInComponent } from './sign-in/sign-in.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [SignInComponent],
   imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
     CommonModule,
     SignInRoutingModule
   ]
